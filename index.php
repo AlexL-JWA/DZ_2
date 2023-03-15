7af391bd5787554acb6fa3c17fc8763975c79913
AlsconWeb <alsconweb_alex@outlook.com>
2023-03-15 19:14:56 +0100

    level 1

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <?php
 
-$numbers = [ 82, 66, 82, 77, 49, 40, 71, 33, 20, 43, ];
+$numbers   = [ 82, 66, 82, 77, 49, 40, 71, 33, 20, 43, ];
+$str_array = [
+	'Создать',
+	'массив',
+	'строк.',
+	'Написать',
+	'функцию',
+	'поиска',
+	'самой',
+	'длинной',
+	'строки',
+	'по',
+	'переданному',
+	'массиву.',
+];
 
-
+/**
+ * Max Numbers.
+ *
+ * @param array $numbers Numbers.
+ *
+ * @return void
+ */
 function max_number( array $numbers ): void {
 	rsort( $numbers );
 
@@ -10,3 +30,20 @@ function max_number( array $numbers ): void {
 }
 
 max_number( $numbers );
+
+/**
+ * Max length string.
+ *
+ * @param array $str_array Array Strings.
+ *
+ * @return void
+ */
+function max_string( array $str_array ): void {
+	$max_string_in_array = array_reduce( $str_array, function ( $carry, $item ) {
+		return mb_strlen( $carry, 'utf-8' ) < mb_strlen( $item, 'utf-8' ) ? $item : $carry;
+	}, '' );
+
+	echo $max_string_in_array . PHP_EOL;
+}
+
+max_string( $str_array );
