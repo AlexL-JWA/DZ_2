bd0c2a434e28f87c25f30a0d085d3a5faaaf1046
AlsconWeb <alsconweb_alex@outlook.com>
2023-03-15 20:07:41 +0100

    level 3

--- index.php
+++ index.php
@@ -1,17 +1,38 @@
 <?php
-
 /**
- * Symbol count.
- *
- * @param string $str String
- *
- * @return void
+ * File name to read
  */
-function symbol_use( string $str ): void {
-	$string = preg_replace( '/[^a-zа-яA-Zа-я]/iu', '', $str );
+const READ_FILE_NAME = 'read.txt';
 
-	echo $string . PHP_EOL;
-	echo strlen( $string ) . PHP_EOL;
+function fizzbuzz( $n ) {
+	return ( $n % 15 === 0 ) ? "fizzbuzz" : ( ( $n % 5 === 0 ) ? "buzz" : ( ( $n % 3 === 0 ) ? "fizz" : $n ) );
 }
 
-symbol_use( 'djhask678.-34  !322выфпапа[216234' );
+/**
+ * Read file to array.
+ *
+ * @param string $file_path File path.
+ *
+ * @return array
+ */
+function read_to_array( string $file_path ): void {
+	$string_array = [];
+
+	$file = fopen( $file_path, 'rb' );
+
+	while ( ! feof( $file ) ) {
+
+		$str = trim( fgets( $file ) );
+
+		$string_array = explode( ', ', $str );
+
+		$fizz_array = array_map( "fizzbuzz", $string_array );
+	}
+
+	fclose( $file );
+
+	echo implode( ', ', $fizz_array ) . PHP_EOL;
+}
+
+
+read_to_array( READ_FILE_NAME );
